55b1e7e9689815c4e7d66929739e5eb22d5fdc64
arlemite47 <nortoynozio19@gmail.com>
2026-04-13 23:21:59 +0500

    Create config.js

---
+++ wiki/.vitepress/config.js
@@ -0,0 +1,21 @@
+export default {
+  title: "Milandur — Wiki",
+  description: "Официальная база знаний Milandur",
+
+  themeConfig: {
+    siteTitle: "Milandur",
+
+    nav: [
+      { text: "Главная", link: "/" }
+    ],
+
+    sidebar: [
+      {
+        text: "Разделы",
+        items: [
+          { text: "Главная", link: "/" }
+        ]
+      }
+    ]
+  }
+}
